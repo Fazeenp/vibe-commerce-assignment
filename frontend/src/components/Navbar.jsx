e75d7be9754fc8ee2c987e24b0857271f9e53d62
Fazeen Patel <144829640+Fazeenp@users.noreply.github.com>
2025-11-07 18:28:25 +0530

    updated the ui

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function Navbar({ cartCount }) {
   return (
-    <nav className="bg-blue-900 text-white p-4 flex justify-between items-center shadow-md">
-      <Link to="/" className="text-xl font-semibold hover:text-gray-300">
-        ShopEasy
+    <nav className="  p-4 flex justify-between items-center shadow-md ">
+      <Link to="/" className="text-2xl pl-10 font-extrabold text-blue-600 hover:text-gray-300">
+        VIBE
       </Link>
 
-      <div className="relative">
+      <div className="relative pr-12">
         <Link to="/cart" className="flex items-center gap-2 hover:text-gray-300">
           <ShoppingCart size={24} />
           <span>Cart</span>
         </Link>
 
         {cartCount > 0 && (
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
+          <span className="absolute -top-2 right-10 bg-red-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
             {cartCount}
           </span>
         )}
